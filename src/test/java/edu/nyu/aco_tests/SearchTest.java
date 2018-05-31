4c99ca1dafdcb416af5f5425a5586152f87b8b61
Anderson Wang <atw293@nyu.edu>
2018-05-31 13:04:37 -0400

    No Repeated SetProperty. Compare minimum and actual number of results. Can't interact with Safari

--- src/test/java/edu/nyu/aco_tests/SearchTest.java
+++ src/test/java/edu/nyu/aco_tests/SearchTest.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 
 //Junits
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,14 +29,15 @@ import org.junit.runners.Parameterized.Parameters;
 //Selectors
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+//Query Class
 class Query{
     protected String browser;
     protected String field;
     protected String scope;
     protected String query;
     protected int minNumResults;
-
 
     Query(String field, String scope, String query, int minNumResults){
         this.field = field;
@@ -53,15 +55,13 @@ class Query{
         this.browser = browser;
     }
 
-    public void printQuery(){
-        System.out.println("SearchTest: " + this.query);
-        System.out.println("Field: " + this.field);
-        System.out.println("Scope: " + this.scope);
-        System.out.println("Number: " + this.minNumResults);
+    @Override
+    public String toString(){
+        return String.format("SearchTest: %s\nField: %s\nScope: %s\nNumber: %d", this.query, this.field, this.scope, this.minNumResults);
     }
 }
 
-
+//Test Class
 @RunWith(Parameterized.class)
 public class SearchTest{
     private Query query;
@@ -69,17 +69,23 @@ public class SearchTest{
     private int timeOut = 10;
     private WebDriverWait wait;
 
-
-    public SearchTest(Query query_data){
-        this.query = query_data;
+    @BeforeClass
+    public static void settingSystemProperties(){
+        System.setProperty("webdriver.chrome.driver", "/usr/local/Cellar/chromedriver/2.35/bin/chromedriver");
+        //	System.setProperty("webdriver.firefox.bin","/Applications/Firefox.app/Contents/MacOS/firefox-bin");
+        System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.19.1/bin/geckodriver");
+        System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
     }
 
-    //Build searches in the paramters. Uses a jagged two-dimensional array
+    //Build searches in the parameters. Uses a jagged two-dimensional array
     @Parameters
     public static ArrayList<Query> query_data() {
         int minNumResultsForKitab = 1352;
         int minNumResultsForNewYorkUniversityLibraries = 1799;
         int minNumResultsForCornell = 742;
+        int minNumResultsForColumbia = 2303;
+        int minNumResultsForPrinceton = 873;
+
         String anyField = "q";
         String title = "title";
         String author = "author";
@@ -91,25 +97,27 @@ public class SearchTest{
         String containsAny = "containsAny";
         String containsAll = "containsAll";
         String matches = "matches";
-//
-//        //List of browsers
-//        String[] browsers = {"Chrome", "Firefox", "Safari"};
 
         //List of queries
         List<Query> queries = Arrays.asList(
                 new Query(anyField, containsAny, "kitab", minNumResultsForKitab),
                 new Query(provider, matches,"New York University Libraries", minNumResultsForNewYorkUniversityLibraries),
-                new Query(provider, containsAny, "Cornell", minNumResultsForCornell));
+                new Query(provider, containsAny, "Cornell", minNumResultsForCornell),
+                new Query(provider, matches, "Columbia University Libraries", minNumResultsForColumbia),
+                new Query(provider, matches, "Princeton Unviersity", minNumResultsForPrinceton)
+        );
 
-        ArrayList<Query> browsers_by_queries = new ArrayList<Query>();
-
+        ArrayList<Query> browsersByQueries = new ArrayList<Query>();
         for(Query query : queries) {
-            browsers_by_queries.add(new Query(query, "Chrome"));
-            browsers_by_queries.add(new Query(query, "Firefox"));
-            browsers_by_queries.add(new Query(query, "Safari"));
+            browsersByQueries.add(new Query(query, "Chrome"));
+            browsersByQueries.add(new Query(query, "Firefox"));
+            browsersByQueries.add(new Query(query, "Safari"));
         }
+        return browsersByQueries;
+    }
 
-        return browsers_by_queries;
+    public SearchTest(Query query_data){
+        this.query = query_data;
     }
 
     @Before
@@ -117,49 +125,18 @@ public class SearchTest{
         String browser = this.query.browser;
         System.out.println("Set Up " + browser);
         if (browser.equals("Chrome")){
-            System.setProperty("webdriver.chrome.driver", "/usr/local/Cellar/chromedriver/2.35/bin/chromedriver");
             driver = new ChromeDriver();
         }
         else if (browser.equals("Firefox")){
-            //	System.setProperty("webdriver.firefox.bin","/Applications/Firefox.app/Contents/MacOS/firefox-bin");
-            System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.19.1/bin/geckodriver");
             driver = new FirefoxDriver();
         }
         else if (browser.equals("Safari")){
-            System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
             driver = new SafariDriver();
         }
         wait = new WebDriverWait(driver, timeOut);
         driver.get("http://dlib.nyu.edu/aco/");
         System.out.println("        Home Page Set! ");
     }
-
-//    @Test
-//    public void testKitab() throws Exception {
-//        try {
-//            Search(anyField, containsAny, "kitab", minNumResultsForKitab);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//    }
-//
-//    @Test
-//    public void testNYU() throws Exception {
-//        try {
-//            Search(provider, matches,"New York University Libraries", minNumResultsForNewYorkUniversityLibraries);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//    }
-
-//    @Test
-//    public void testCornell() throws Exception{
-//        try {
-//            Search(provider, containsAny, "Cornell", minNumResultsForCornell);
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//    }
 
     @Test
     public void runTest() throws Exception{
@@ -177,7 +154,7 @@ public class SearchTest{
     }
 
     public void Search(){
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("search_holder_advanced")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("submit-hold")));
         Select fieldBox = new Select(driver.findElement(By.className("field-select")));
         fieldBox.selectByValue(query.field);
 
@@ -188,23 +165,19 @@ public class SearchTest{
         actions.moveToElement(driver.findElement(By.className("input-hold")));
         actions.click();
         actions.sendKeys(query.query);
+        actions.moveToElement(driver.findElement(By.className("submit-hold")));
+        actions.click();
         actions.build().perform();
 
-        System.out.println("       Searched ");
-        query.printQuery();
+        System.out.println("       Searching ");
+        System.out.println(query);
         //Check result count
         wait.until(ExpectedConditions.presenceOfElementLocated(By.className("resultsnum")));
         String numFound = driver.findElement(By.cssSelector("div[class='resultsnum'] span[class='numfound']")).getText();
-        int numberOfResults = Integer.parseInt(numFound);
 
-        boolean greaterThanMin;
-        if(numberOfResults >= query.minNumResults) {
-            greaterThanMin = true;
-        }
-        else {
-            greaterThanMin = false;
-        }
-        assertTrue(greaterThanMin);
+        int numberOfResults = Integer.parseInt(numFound);
+        System.out.print(numberOfResults);
+        assertTrue(numberOfResults >= query.minNumResults);
         System.out.println("     PASS");
     }
 }
