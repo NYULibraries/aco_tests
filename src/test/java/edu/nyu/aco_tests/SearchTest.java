b7119ac9f5787626666cfaadefac3a751277fa69
Anderson Wang <atw293@nyu.edu>
2018-06-22 13:56:56 -0400

    Removed unused WebElement is Present/Visible methods

--- src/test/java/edu/nyu/aco_tests/SearchTest.java
+++ src/test/java/edu/nyu/aco_tests/SearchTest.java
@@ -128,17 +128,5 @@ public class SearchTest{
                 "; got " + numberOfResults + " number of results",numberOfResults >= query.minNumResults);
     }
 
-    public boolean isElementPresent(By locatorKey) {
-        try {
-            driver.findElement(locatorKey);
-            return true;
-        } catch (org.openqa.selenium.NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public boolean isElementVisible(String cssLocator){
-        return driver.findElement(By.cssSelector(cssLocator)).isDisplayed();
-    }
 }
 
